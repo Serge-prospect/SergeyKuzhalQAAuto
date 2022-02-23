4c7968c75b4d6aa2a62861a5b103886138b0f3bc
Serge-prospect <prospect@tut.by>
2022-02-24 00:34:34 +0300

    HW02 - Add Array10toSquaredArray app

---
+++ Array10toSquaredArray/Array10toSquaredArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array10toSquaredArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] initialArray = new int[10];
+            for (int index = 0; index < initialArray.Length; index++)
+            {
+                initialArray[index] = index;
+            }
+            Console.WriteLine("Initial array indexes: [{0}]", string.Join(", ", initialArray));
+
+            int[] squaredArray = initialArray;
+            for (int index = 0; index < squaredArray.Length; index++)
+            {
+                squaredArray[index] = (int)Math.Pow(initialArray[index], 2);
+            }
+            Console.WriteLine("Squared array indexes: [{0}]", string.Join(", ", squaredArray));
+        }
+    }
+}
