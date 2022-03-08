092f6086e1bfec08d55edc38b52a52da5cca5d66
Serge-prospect <prospect@tut.by>
2022-03-08 14:59:27 +0300

    Remove extra variables (min/max datatype). Refactoring of Console.Writeline (remove concatination)

--- MinMaxTypeValues/MinMaxTypeValues/Program.cs
+++ MinMaxTypeValues/MinMaxTypeValues/Program.cs
@@ -7,91 +7,60 @@ namespace MinMaxTypeValues
         static void Main(string[] args)
         {
             string byteType = "BYTE";
-            byte byteMinValue = byte.MinValue;
-            byte byteMaxValue = byte.MaxValue;
-
             string shortType = "SHORT";
-            short shortMinValue = short.MinValue;
-            short shortMaxValue = short.MaxValue;
-
             string intType = "INT";
-            int intMinValue = int.MinValue;
-            int intMaxValue = int.MaxValue;
-
             string longType = "LONG";
-            long longMinValue = long.MinValue;
-            long longMaxValue = long.MaxValue;
-
             string floatType = "FLOAT";
-            float floatMinValue = float.MinValue;
-            float floatMaxValue = float.MaxValue;
-
             string doubleType = "DOUBLE";
-            double doubleMinValue = double.MinValue;
-            double doubleMaxValue = double.MaxValue;
-
             string uintType = "UINT";
-            uint uintMinValue = uint.MinValue;
-            uint uintMaxValue = uint.MaxValue;
-
             string ulongType = "ULONG";
-            ulong ulongMinValue = ulong.MinValue;
-            ulong ulongMaxValue = ulong.MaxValue;
 
             Console.WriteLine
                 (
-                    "Minimal {0} data type value = {1},\n" +
-                    "Maximal {0} data type value = {2}.\n",
-                    byteType, byteMinValue, byteMaxValue
+                    "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
+                    byteType, byte.MinValue, byte.MaxValue
                 );
 
             Console.WriteLine
                 (
-                    "Minimal {0} data type value = {1},\n" +
-                    "Maximal {0} data type value = {2}.\n",
-                    shortType, shortMinValue, shortMaxValue
+                    "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
+                    shortType, short.MinValue, short.MaxValue
                 );
 
             Console.WriteLine
                 (
-                    "Minimal {0} data type value = {1},\n" +
-                    "Maximal {0} data type value = {2}.\n",
-                    intType, intMinValue, intMaxValue
+                    "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
+                    intType, int.MinValue, int.MaxValue
                 );
 
             Console.WriteLine
                 (
-                    "Minimal {0} data type value = {1},\n" +
-                    "Maximal {0} data type value = {2}.\n",
-                    longType, longMinValue, longMaxValue
+                    "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
+                    longType, long.MinValue, long.MaxValue
                 );
 
             Console.WriteLine
                 (
-                    "Minimal {0} data type value = {1},\n" +
-                    "Maximal {0} data type value = {2}.\n",
-                    floatType, floatMinValue, floatMaxValue
+                    "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
+                    floatType, float.MinValue, float.MaxValue
                 );
 
             Console.WriteLine
                 (
-                    "Minimal {0} data type value = {1},\n" +
-                    "Maximal {0} data type value = {2}.\n",
-                    doubleType, doubleMinValue, doubleMaxValue
+                    "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
+                    doubleType, double.MinValue, double.MaxValue
                 );
 
             Console.WriteLine
                 (
-                    "Minimal {0} data type value = {1},\n" +
-                    "Maximal {0} data type value = {2}.\n",
-                    uintType, uintMinValue, uintMaxValue
+                    "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
+                    uintType, uint.MinValue, uint.MaxValue
                 );
 
             Console.WriteLine
                 (
-                    "Minimal {0} data type value = {1},\n" +
-                    "Maximal {0} data type value = {2}.\n",
-                    ulongType, ulongMinValue, ulongMaxValue
+                    "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
+                    ulongType, ulong.MinValue, ulong.MaxValue
                 );
         }
     }
