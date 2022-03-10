274a8a673680341456e78ad490fe6757e7f30aa6
Serge-prospect <prospect@tut.by>
2022-03-10 10:14:12 +0300

    Remove data type variables

--- MinMaxTypeValues/MinMaxTypeValues/Program.cs
+++ MinMaxTypeValues/MinMaxTypeValues/Program.cs
@@ -6,61 +6,52 @@ namespace MinMaxTypeValues
     {
         static void Main(string[] args)
         {
-            string byteType = "BYTE";
-            string shortType = "SHORT";
-            string intType = "INT";
-            string longType = "LONG";
-            string floatType = "FLOAT";
-            string doubleType = "DOUBLE";
-            string uintType = "UINT";
-            string ulongType = "ULONG";
-
             Console.WriteLine
                 (
                     "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
-                    byteType, byte.MinValue, byte.MaxValue
+                    byte.MinValue.GetType().Name, byte.MinValue, byte.MaxValue
                 );
 
             Console.WriteLine
                 (
                     "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
-                    shortType, short.MinValue, short.MaxValue
+                    short.MinValue.GetType().Name, short.MinValue, short.MaxValue
                 );
 
             Console.WriteLine
                 (
                     "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
-                    intType, int.MinValue, int.MaxValue
+                    int.MinValue.GetType().Name, int.MinValue, int.MaxValue
                 );
 
             Console.WriteLine
                 (
                     "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
-                    longType, long.MinValue, long.MaxValue
+                    long.MinValue.GetType().Name, long.MinValue, long.MaxValue
                 );
 
             Console.WriteLine
                 (
                     "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
-                    floatType, float.MinValue, float.MaxValue
+                    float.MinValue.GetType().Name, float.MinValue, float.MaxValue
                 );
 
             Console.WriteLine
                 (
                     "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
-                    doubleType, double.MinValue, double.MaxValue
+                    double.MinValue.GetType().Name, double.MinValue, double.MaxValue
                 );
 
             Console.WriteLine
                 (
                     "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
-                    uintType, uint.MinValue, uint.MaxValue
+                    uint.MinValue.GetType().Name, uint.MinValue, uint.MaxValue
                 );
 
             Console.WriteLine
                 (
                     "Minimal {0} data type value = {1},\nMaximal {0} data type value = {2}.\n",
-                    ulongType, ulong.MinValue, ulong.MaxValue
+                    ulong.MinValue.GetType().Name, ulong.MinValue, ulong.MaxValue
                 );
         }
     }
