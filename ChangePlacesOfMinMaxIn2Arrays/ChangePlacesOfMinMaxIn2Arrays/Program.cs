dc74a3921d500e24bd6993ea262bd99253a26da5
Serge-prospect <prospect@tut.by>
2022-03-15 01:54:11 +0300

    Big refactoring for optimisation. Added PackageReference Include="IndexRange" Version="1.0.1"

--- ChangePlacesOfMinMaxIn2Arrays/ChangePlacesOfMinMaxIn2Arrays/Program.cs
+++ ChangePlacesOfMinMaxIn2Arrays/ChangePlacesOfMinMaxIn2Arrays/Program.cs
@@ -3,11 +3,11 @@
 namespace ChangePlacesOfMinMaxIn2Arrays
 {
     class Program
-    {
+    {       
         /// <summary>
-        /// Create new array by params values 
+        /// Create new array by values
         /// </summary>
-        /// <param name="arrayVallues">New array values</param>
+        /// <param name="arrayVallues">Array values</param>
         /// <returns>array</returns>
         static int[] CreateArray(params int[] arrayVallues)
         {
@@ -16,96 +16,66 @@ namespace ChangePlacesOfMinMaxIn2Arrays
         }
 
         /// <summary>
-        /// Get min array value
+        /// Get min/max array values and return as array, where minMaxArrayValues[0] = min, minMaxArrayValues[1] = max
         /// </summary>
         /// <param name="array">Array</param>
-        /// <returns>minArrayValue</returns>
-        static int GetMinArrayValue(int[] array)
+        /// <returns>minMaxArrayValues</returns>
+        static int[] GetMinMaxArrayValues(int[] array)
         {
-            int minArrayValue = array[0];
-            for (int index = 1; index < array.Length; index++)
-            {
-                minArrayValue = minArrayValue <= array[index] ? minArrayValue : array[index];
-            }
-            return minArrayValue;
+            int[] sortedArray = new int[array.Length];
+            Array.Copy(array, sortedArray, array.Length);
+            Array.Sort(sortedArray);
+
+            int minArrayValue = sortedArray[0];
+            int maxArrayValue = sortedArray[^1];
+            int[] minMaxArrayValues = new int[] { minArrayValue, maxArrayValue };
+
+            return minMaxArrayValues;
         }
 
         /// <summary>
-        /// Get max array value
-        /// </summary>
-        /// <param name="array">Array</param>
-        /// <returns>maxArrayValue</returns>
-        static int GetMaxArrayValue(int[] array)
-        {
-            int maxArrayValue = array[0];
-            for (int index = 1; index < array.Length; index++)
-            {
-                maxArrayValue = maxArrayValue >= array[index] ? maxArrayValue : array[index];
-            }
-            return maxArrayValue;
-        }
-
-        /// <summary>
-        /// Get changed array with replaced min <-> max values
+        /// Get array with changed min <-> max value places
         /// </summary>
         /// <param name="array">Initial array</param>
-        /// <param name="minArrayValue">Minimal array value</param>
-        /// <param name="maxArrayValue">Maximal array value</param>
         /// <returns>changedMinMaxArray</returns>
-        static int[] GetChangedMinMaxArray(int[] array, int minArrayValue, int maxArrayValue)
+        static int[] GetChangedMinMaxArray(int[] array)
         {
-            int[] changedMinMaxArray = new int[array.Length];            
-            for (int index = 0; index < array.Length; index++)
-            {
-                //Change min to max
-                if (array[index] == minArrayValue)
-                {
-                    changedMinMaxArray[index] = maxArrayValue;
-                }
-                //Change max to min
-                else if (array[index] == maxArrayValue)
-                {
-                    changedMinMaxArray[index] = minArrayValue;
-                }
-                else
-                {
-                    changedMinMaxArray[index] = array[index];
-                }
-            }            
+            int[] changedMinMaxArray = new int[array.Length];
+            Array.Copy(array, changedMinMaxArray, array.Length);
+
+            int minArrayValue = GetMinMaxArrayValues(array)[0];
+            int maxArrayValue = GetMinMaxArrayValues(array)[1];
+
+            int minArrayValueIndex = Array.IndexOf(array, minArrayValue);
+            int maxArrayValueIndex = Array.IndexOf(array, maxArrayValue);
+
+            changedMinMaxArray[minArrayValueIndex] = maxArrayValue;
+            changedMinMaxArray[maxArrayValueIndex] = minArrayValue;
+      
             return changedMinMaxArray;
         }
 
         /// <summary>
-        /// Display array info: initial array, min aaray value, max array value, changed array with replaced min <-> max values 
+        /// Display array info: initial array, min aaray value, max array value, changed array with replaced min <-> max values
         /// </summary>
         /// <param name="arrayNumber">Array number</param>
         /// <param name="array">Initial array</param>
-        /// <param name="minArrayValue">Minimal array value</param>
-        /// <param name="maxArrayValue">Maximal array value</param>
-        /// <param name="changedMinMaxArray">Changed array with replaced min <-> max values</param>
-        static void DisplayArrayInfo(int arrayNumber, int[] array, int minArrayValue, int maxArrayValue, int[] changedMinMaxArray)
+        static void DisplayArrayInfo(int arrayNumber, int[] array)
         {
+            int minArrayValue = GetMinMaxArrayValues(array)[0];
+            int maxArrayValue = GetMinMaxArrayValues(array)[1];
+            int[] changedMinMaxArray = GetChangedMinMaxArray(array);
+
             Console.WriteLine("Array #{0}: [{1}]", arrayNumber, String.Join(", ", array));
             Console.WriteLine("Minimal array #{0} value: {1}", arrayNumber, minArrayValue);
             Console.WriteLine("Maximal array #{0} value: {1}", arrayNumber, maxArrayValue);
             Console.WriteLine("Changed min <-> max places in array #{0}: [{1}]", arrayNumber, String.Join(", ", changedMinMaxArray));
         }
+
         static void Main(string[] args)
         {
-            int[] array1 = CreateArray(10, 1, 5, -4, 6, 32, 8);
-            int[] array2 = CreateArray(36, 258, -39, -40, -600);
-
-            int minArray1Value = GetMinArrayValue(array1);
-            int minArray2Value = GetMinArrayValue(array2);
-
-            int maxArray1Value = GetMaxArrayValue(array1);
-            int maxArray2Value = GetMaxArrayValue(array2);
-
-            int[] changedMinMaxArray1 = GetChangedMinMaxArray(array1, minArray1Value, maxArray1Value);
-            int[] changedMinMaxArray2 = GetChangedMinMaxArray(array2, minArray2Value, maxArray2Value);
-
-            DisplayArrayInfo(1, array1, minArray1Value, maxArray1Value, changedMinMaxArray1);
-            DisplayArrayInfo(2, array2, minArray2Value, maxArray2Value, changedMinMaxArray2);
+            DisplayArrayInfo(1, CreateArray(10, 1, 5, -4, 6, 32, 8));
+            DisplayArrayInfo(2, CreateArray(36, 258, -39, -40, -600));
         }
     }
 }
