8cb7542389a3f7a884473dca5f3ae95e32b50ce1
Serge-prospect <prospect@tut.by>
2022-04-01 23:09:41 +0300

    HW06 - Add ConsumerPayments app

---
+++ ConsumerPayments/ConsumerPayments/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsumerPayments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consumers = new Consumer[5];
+
+            consumers[0] = new ConsumerCash("John", "Dore");
+            consumers[1] = new ConsumerDebit("Kate", "Brown");
+            consumers[2] = new ConsumerCredit("Jane", "Clark");
+            consumers[3] = new ConsumerCredit("Mark", "Smith");
+            consumers[4] = new ConsumerCash("Ben", "Gun");
+
+            foreach(var consumer in consumers)            
+                consumer.Pay();            
+        }
+    }
+}
